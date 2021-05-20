889f3534af01f1f60457e019ee4a55efac00b757
amelsalah <76889246+amelsalah@users.noreply.github.com>
2021-05-20 15:15:10 +0200

    Add files via upload

---
+++ custom_portal_account_page/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "custom_portal_account_page",
+
+    'summary': """
+        This Module change Portal Account page design with changing profile picture feature.
+        """,
+
+  
+
+    'author': "Amel Salah",
+    'website': "https://github.com/amelsalah",
+
+    'category': 'portal',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','portal'],
+
+    # always loaded
+    'data': [
+
+        'views/templates.xml',
+
+    ],
+
+
+}
